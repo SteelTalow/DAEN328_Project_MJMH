e971489c2257c6e7d90a67efa3c49d26e259861d
jamdadamT <jamdadam@tamu.edu>
2025-04-15 13:52:34 -0500

    added to .env file

--- load.py
+++ load.py
@@ -1,9 +1,24 @@
 import pandas as pd
 from sqlalchemy import create_engine
+import os
+import pandas as pd
+import psycopg2
 
 def load_postgress(taxi_data):
-    #working here
-    print()
+    # Load env vars
+    DB_HOST = os.getenv("POSTGRES_HOST")
+    DB_PORT = os.getenv("POSTGRES_PORT")
+    DB_USER = os.getenv("POSTGRES_USER")
+    DB_PASS = os.getenv("POSTGRES_PASSWORD")
+    DB_NAME = os.getenv("POSTGRES_DB")
+
+    db_url = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+    engine = create_engine(db_url)
+
+    df = pd.read_csv("/app/data/cleaned_taxidata.csv")
+
+    
+
 
 
 
